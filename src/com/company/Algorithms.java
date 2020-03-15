a1c939b0e5cb1376b958f47ee24b2a799776fb70
shuzhou li <shuzhou.li@ssfs.org>
2020-03-15 15:59:00 -0400

    all fixed

--- src/com/company/Algorithms.java
+++ src/com/company/Algorithms.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -11,15 +12,15 @@ public class Algorithms {
         Scanner file2 = new Scanner(new File("file2.txt"));
         ArrayList<Integer> f1 = new ArrayList<Integer>();
         ArrayList<Integer> f2 = new ArrayList<Integer>();
-        for(int i=0;i<20;i++){
+        while (file1.hasNext()){
             f1.add(file1.nextInt());
         }
-        for(int i=0;i<20;i++){
+        while (file2.hasNext()){
             f2.add(file2.nextInt());
         }
         ArrayList<Integer> ret = new ArrayList<Integer>();
-        for(int i=0;i<20;i++){
-            for(int j=0;j<20;j++){
+        for(int i=0;i<f1.size();i++){
+            for(int j=0;j<f2.size();j++){
                 if(f1.get(i)==f2.get(j)){
                     ret.add(f2.get(j));
                 }
@@ -27,42 +28,39 @@ public class Algorithms {
         }
         return ret;
     }
-    public static ArrayList<String> fileDuplicatesTwo()throws IOException{
+    public static ArrayList<String> fileDuplicatesTwo() throws FileNotFoundException {
         Scanner names = new Scanner(new File("names.txt"));
-       String ori="";
        ArrayList<String> ret= new ArrayList<String>();
-       int count=0;
         while(names.hasNext()){
-            ori+=names.next();
             ret.add(names.next());
-            count++;
         }
-        System.out.println(ori);
-        for(int i=0;i<count;i++){
-            for(int j=0;j<count;j++){
-                if(ret.get(i)==ret.get(j)){
-                    ret.remove(i);
+        System.out.println(ret);
+        for(int i=0;i<ret.size()-1;i++){
+            for(int j=i+1;j<ret.size();j++){
+                if(ret.get(i).equals(ret.get(j))){
                     ret.remove(j);
-                    i--;
-                    j--;
                 }
             }
         }
         return ret;
     }
     public static ArrayList<Integer> orderedList()throws IOException{
-        Scanner file1 = new Scanner(new File("com/company/file1.txt"));
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        for(int i=1;i<20;i++){
+        Scanner file1 = new Scanner(new File("file1.txt"));
+        ArrayList<Integer> ret = new ArrayList<Integer>();
+        ret.add(file1.nextInt());
+        while(file1.hasNext()){
             int cur=file1.nextInt();
-            int j=i-1;
-            int num=file1.nextInt();
-            while(j>=0&&cur< num){
-                list.set(j+1, num);
-                j--;
+            for(int i=0;i<=ret.size()-1;i++){
+                if(cur<ret.get(i)){
+                    ret.add(i,cur);
+                    break;
+                }
+                if(i==ret.size()-1){
+                    ret.add(cur);
+                    break;
+                }
             }
-            list.set(j+1,cur);
         }
-        return list;
+        return ret;
     }
 }
